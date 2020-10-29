21758bd523f72eb53815b39c7eeb84b45f762a33
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-10-28 18:14:48 -0600

    enable compress and block size capabilities

--- app.py
+++ app.py
@@ -9,27 +9,64 @@ venti_index = {}
 venti_log = []
 venti_files = {}
 venti_changes = []
+total_sizes = []
+real_sizes = []
+
+
+def update_sizes(total_delta, real_delta):
+    total_size = total_sizes[-1] if len(total_sizes) > 0 else 0
+    real_size = real_sizes[-1] if len(real_sizes) > 0 else 0
+    total_sizes.append(total_size + total_delta)
+    real_sizes.append(real_size + real_delta)
 
 
 @app.route('/',  methods=['GET'])
 def index():
     return render_template('index.html',
-                           log=venti_log, index=venti_index, files_dict=venti_files, changes=venti_changes)
+                           log=venti_log, index=venti_index,
+                           files_dict=venti_files, changes=venti_changes,
+                           total_size=total_sizes[-1] if len(total_sizes) > 0 else 0,
+                           real_size=real_sizes[-1] if len(real_sizes) > 0 else 0)
 
 
 @app.route('/archive/', methods=['POST'])
 def archive():
     if request.files:
         files = request.files.getlist("uploaded_files")
+
+        compress = request.form.get("enable_compression")
+        compress = (compress == 'on') if compress else False
+
+        block_size = request.form.get("block_size")
+        block_size = int(block_size) * 1024 if block_size else 4096
+
         venti_changes.clear()
+
         for file in files:
             file_path = f'./temporal_files/{file.filename}'
             file.save(file_path)
-            fingerprints, changes = archive_file(file_path, venti_log, venti_index)
+            fingerprints, changes, total_delta, real_delta = archive_file(file_path=file_path,
+                                                                          log=venti_log,
+                                                                          index=venti_index,
+                                                                          compress=compress,
+                                                                          block_size=block_size)
             venti_files[file.filename] = fingerprints
+            update_sizes(total_delta, real_delta)
             for change in changes:
                 venti_changes.append(change)
             os.remove(file_path)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete_log/', methods=['GET'])
+def delete_log():
+    venti_index.clear()
+    venti_log.clear()
+    venti_files.clear()
+    venti_changes.clear()
+    total_sizes.clear()
+    real_sizes.clear()
 
     return redirect(url_for('index'))
 
--- venti.py
+++ venti.py
@@ -1,34 +1,44 @@
 from hashlib import sha1
+import sys
 import zlib
 
 
 def archive_file(file_path, log, index, compress=False, block_size=4096):
     fingerprints = []
     block_changes = []
+    total_size = 0
+    real_size = 0
     f = open(file_path, 'rb')
 
     block = f.read(block_size)
     while block:
+        total_size += size_in_kb(block)
         fingerprint = get_fingerprint(block)
         fingerprints.append(fingerprint)
+
+        if compress:
+            block = zlib.compress(block)
 
         if fingerprint in index:
             block_changes.append((fingerprint, block, False))
         else:
-            if compress:
-                block = zlib.compress(block)
             log.append(block)
             index[fingerprint] = len(log) - 1
             block_changes.append((fingerprint, block, True))
+            real_size += size_in_kb(block)
 
         block = f.read(block_size)
 
     f.close()
 
-    return fingerprints, block_changes
+    return fingerprints, block_changes, total_size, real_size
 
 
 def get_fingerprint(block):
     digestor = sha1()
     digestor.update(block)
     return digestor.hexdigest()
+
+
+def size_in_kb(block):
+    return sys.getsizeof(block) / 1024
